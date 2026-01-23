9d8b4a18e587521208c70de4e846f18e9e38cbb3
IrmaIvanova <nowa.mir@yandex.ru>
2026-01-23 18:10:27 +0300

    refactor(code-examples): restructure module page layout

--- services/examples/src/components/App.tsx
+++ services/examples/src/components/App.tsx
@@ -1,31 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import '../styles/tailwind.css'
-import { Container, CustomLink, ThemeToggle, Typography } from '@packages/shared/src';
+import { Container, CustomLink, Section, ThemeToggle, Typography } from '@packages/shared/src';
 import { UsersList } from '@/pages/usersList';
 import { codeExamplesRoutes } from '@packages/shared/src/routes/code-examples';
 import { Cards } from '@/pages/cards/Cards';
+import { useLocation, useParams } from "react-router-dom";
 
 
 export const App: React.FC = () => {
+    const loc = useParams();
 
-
-
+    console.log("loc", loc)
     return (
-        <Container>
+        <Container >
             <ThemeToggle />
 
-            <Typography variant='h1'>
-                code-examples MODULE
-
-            </Typography>
-
-            <CustomLink to={codeExamplesRoutes.main} variant='primary' nav children='Пример списка пользователей' />
+            <Section className='flex'>
+                <Section>
+                    <Typography variant="h3" children={"Примеры кода"} className='my-8' />
 
 
-            <Typography variant="h3" children={"карточки по темам"} className='my-8' />
+                    <CustomLink to={codeExamplesRoutes.main} variant='primary' nav children='Пример списка пользователей' />
 
-            <Cards />
+                </Section>
+                <Section>
+
+                    <Typography variant="h3" children={"карточки по темам"} className='my-8' />
+
+                    <Cards />
+                </Section>
+            </Section>
             <Outlet />
         </Container>
     )
